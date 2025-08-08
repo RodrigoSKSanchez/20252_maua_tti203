1a24316dfbcbc6d68133ac4a5d7d9d9ce7555a6e
Rodrigo Seizo Kawakami Sanchez <24.95012-2@maua.br>
2025-08-08 12:33:28 -0300

    aula 08/08/25

---
+++ projetos_dart/introducao.dart
@@ -0,0 +1,58 @@
+void main() {
+
+    // int a = 2;
+    // ++a;
+    // print(a);
+    // a++;
+    // print(a);
+    // print(++a);
+    // print(a++);
+    // print(a);
+
+
+
+
+    // String idadeTextual = '25';
+    // int idade = int.parse(idadeTextual);
+    // print(idade);
+    // String pesoTextual = '85.2';
+    // double peso = double.parse(pesoTextual);
+    // print(peso);
+    // String alturaTextual = '1.8';
+    // num altura = num.parse(alturaTextual);
+    // print(altura);
+    // print(altura.runtimeType);
+
+    // String nome = 'João';
+    // String sobrenome = 'Silva';
+    // String endereco = '''
+    // Rua B,
+    // número 1234, Vila J
+    // ''';
+    // int idade = 20;
+    // print('Me chamo $nome');
+    // print('Tenho $idade anos');
+    // print('Ano que vem terei ${idade + 1} anos');
+
+
+    // print('Me chamo ' + nome + ' ' + sobrenome);
+    // print('Tenho ' + idade.toString() + ' de idade');
+
+    // int a = 2;
+    // double b = 3.5;
+    // String c = "abc";
+    // bool d = true;
+    // num e = 2;
+    // num f = 2.2;
+    // print(a.runtimeType);
+    // print(b.runtimeType);
+    // print(c.runtimeType);
+    // print(d.runtimeType);
+    // print(e.runtimeType);
+    // print(f.runtimeType);
+}
+
+// void main() {
+//     //systen.out.println
+//     print("Hello World");
+// }
